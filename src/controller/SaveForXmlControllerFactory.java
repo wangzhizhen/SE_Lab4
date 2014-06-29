b124ef6cbccd29587cae87712968b0683d4e29ef
qujiaqing <jiaqing_qu@yeah.net>
2014-06-30 01:19:37 +0800

    Revert "track new files"
    This reverts commit 95754c610f8ce91fc9c57cf59c715e6b1a6c061a.

--- src/controller/SaveForXmlControllerFactory.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class SaveForXmlControllerFactory {
-	public ISaveController getSaveController(int letterPosition) {
-		// TODO Auto-generated method stub
-		return new SaveForXmlController(letterPosition);
-	}
-}
